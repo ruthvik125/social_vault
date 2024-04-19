12458b5dc31126925e67f1101c9e15be54b02e89
ruthvik125 <ruthvik.mitmpl2022@learner.manipal.edu>
2024-04-19 16:23:29 +0530

    login validation

--- assets/pages/login.php
+++ assets/pages/login.php
@@ -1,34 +1,31 @@
 
     <div class="login">
         <div class="col-sm-12 col-md-4 bg-white border rounded p-4 shadow-sm">
-            <form method="post" action="assets/php/actions.php?login">
-                <div class="d-flex justify-content-center">
+        <form method="post" action="assets/php/actions.php?login">
+    <div class="d-flex justify-content-center">
+        <img class="mb-4" src="assets/images/pictogram.png" alt="" height="45">
+    </div>
+    <h1 class="h5 mb-3 fw-normal">Please sign in</h1>
 
-                    <img class="mb-4" src="assets/images/pictogram.png" alt="" height="45">
-                </div>
-                <h1 class="h5 mb-3 fw-normal">Please sign in</h1>
-
-                <div class="form-floating">
-                    <input type="text" name="username_email" value="<?=showFormData('username_email')?>" class="form-control rounded-0" placeholder="username/email">
-                    <label for="floatingInput">username/email</label>
-                </div>
-                <?=showError('username_email')?>
-                <div class="form-floating mt-1">
-                    <input type="password" name="password" class="form-control rounded-0" id="floatingPassword" placeholder="Password">
-                    <label for="floatingPassword">password</label>
-                </div>
-                <?=showError('password')?>
-                <?=showError('checkuser')?>
-
-
-                <div class="mt-3 d-flex justify-content-between align-items-center">
-                    <button class="btn btn-primary" type="submit">Sign in</button>
-                    <a href="?signup" class="text-decoration-none">Create New Account</a>
+    <div class="form-floating">
+        <input type="text" name="username_email" value="<?=showFormData('username_email')?>" class="form-control rounded-0" placeholder="username/email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required>
+        <label for="floatingInput">username/email</label>
+    </div>
+    <?=showError('username_email')?>
+    <div class="form-floating mt-1">
+        <input type="password" name="password" class="form-control rounded-0" id="floatingPassword" placeholder="Password" pattern=".{8,}" title="Password must be at least 8 characters long" required>
+        <label for="floatingPassword">password</label>
+    </div>
+    <?=showError('password')?>
+    <?=showError('checkuser')?>
 
 
-                </div>
-                
-            </form>
+    <div class="mt-3 d-flex justify-content-between align-items-center">
+        <button class="btn btn-primary" type="submit">Sign in</button>
+        <a href="?signup" class="text-decoration-none">Create New Account</a>
+    </div>
+</form>
+
         </div>
     </div>
 
